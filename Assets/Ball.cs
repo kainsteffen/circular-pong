bf800672e9d789555e394046ddddbae364433cf0
kainsteffen <dayturnkain@gmail.com>
2019-10-15 22:04:43 +0700

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float initSpeed;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(0, -initSpeed), ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float turnSpeed;
+    public Text scoreText;
+    public int score;
+    public float timeOutLength;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = timeOutLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer -= Time.deltaTime;
+        if(timer < 0 )
+        {
+            scoreText.text = "Game Over";
+        } else
+        {
+            scoreText.text = score.ToString();
+        }
+
+        
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(new Vector3(0, 0, -Time.deltaTime * turnSpeed));
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(new Vector3(0, 0, Time.deltaTime * turnSpeed));
+        }
+
+        if (Input.GetKey("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        score++;
+        timer = timeOutLength;
+    }
+}
